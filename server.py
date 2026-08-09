a631626bf11480245a4107ee1591a6f7b404539d
AlokShrishrimal <alokshrishrimal2@gmail.com>
2026-08-09 16:36:31 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,3 @@
-from flask import Flask, request
-from emotion_detection import emotion_detector
-
-app = Flask(__name__)
-
 @app.route("/emotionDetector")
 def emotion():
     text_to_analyze = request.args.get("textToAnalyze")
@@ -12,10 +7,3 @@ def emotion():
 
     result = emotion_detector(text_to_analyze)
     return str(result)
-
-@app.route("/")
-def home():
-    return "Emotion Detector Application"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
