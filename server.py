8d567ebf89ba95abfc6ec2035ae520a871cc82f9
AlokShrishrimal <alokshrishrimal2@gmail.com>
2026-08-09 16:11:50 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route("/emotionDetector")
 def emotion():
     text_to_analyze = request.args.get("textToAnalyze")
+
+    if text_to_analyze is None or text_to_analyze.strip() == "":
+        return "Invalid text! Please try again!", 400
+
     result = emotion_detector(text_to_analyze)
     return str(result)
 
